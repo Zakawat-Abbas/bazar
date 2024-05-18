844164f0578358e8855f9ba36ef98863c315d4a8
Zakawat Abbas <zakawatabbas123@gmail.com>
2024-05-18 21:02:19 +0500

    Finished

--- client/src/fireabase.config.js
+++ client/src/fireabase.config.js
@@ -5,12 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: `${process.env.Firebase_KEY}`,
+  apiKey: "AIzaSyDK3ujZZSle62lddf2HASj289WRgkiz458",
   authDomain: "bazar-75bf4.firebaseapp.com",
   projectId: "bazar-75bf4",
   storageBucket: "bazar-75bf4.appspot.com",
   messagingSenderId: "617519812679",
   appId: "1:617519812679:web:656030c5c8d4e17598e162"
 };
+
+console.log(firebaseConfig);
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
