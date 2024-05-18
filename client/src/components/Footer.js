e929fe4f08743c29b0d69aa93b77bd83d47d6241
Zakawat Abbas <zakawatabbas123@gmail.com>
2024-05-18 23:48:12 +0500

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -17,7 +17,6 @@ const Footer = () => {
       <div className="max-w-screen-xl mx-auto grid grid-cols-4">
         <div className="flex flex-col gap-7">
           <img className="w-32" src={logoLight} alt="logoLight" />
-          // <p className="text-white text-sm tracking-wide">© ReactBD.com</p>
           <img className="w-56" src={paymentLogo} alt="paymentLogo" />
           <div className="flex gap-5 text-lg text-gray-400">
             <ImGithub className="hover:text-white duration-300 cursor-pointer" />
@@ -30,10 +29,10 @@ const Footer = () => {
         <div>
           <h2 className="text-2xl font-semibold text-white mb-4">locate us</h2>
           <div className="text-base flex flex-col gap-2">
-            <p>Karachi, Pakistan</p>
-            <p>Mobile: 03423235676</p>
-            // <p>Phone: 00968 24769821</p>
-            <p>e-mail: zakawatabbas123@gmail.com</p>
+            <p>MBD,Ruwi, Muscat-Oman</p>
+            <p>Mobile: 00968 97859628</p>
+            <p>Phone: 00968 24769821</p>
+            <p>e-mail: bazar@gmail.com</p>
           </div>
         </div>
         <div>
